287d23b9db4186178b5c54f28d4347db1276656a
spindyzel <40053870+spindyzel@users.noreply.github.com>
2019-03-22 21:35:05 +0700

    Update Find Extra One.cpp

--- Find Extra One.cpp
+++ Find Extra One.cpp
@@ -1,6 +1,21 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
+
 using namespace std;
 
-int main(){
-
+int main() {
+  int n;
+  scanf("%d", &n);
+  int le = 0, ri = 0;
+  for (int i = 0; i < n; ++i) {
+    int x;
+    scanf("%d %*d", &x);
+    if (x < 0) le++;
+    else ri++;
+  }
+  if (min(le, ri) <= 1) {
+    puts("Yes");
+  }
+  else
+    puts("No");
+  return 0;
 }
